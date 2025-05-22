bdf7893e25399c8066738494f6d31aad7e44c393
Angel Luis <angel.luis.freelancer@gmail.com>
2025-05-22 15:52:39 +0200

    cierre de sesiones finalizado. next step: iniciar con los test

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from .config import init_app
+from sqlalchemy.orm import scoped_session, sessionmaker
 
+from .config import db, init_app
 from .routes import main_bp, api_bp
 
 def create_app(config_class=None):
@@ -11,7 +12,24 @@ def create_app(config_class=None):
         from app.config.settings import Config
         app.config.from_object(Config)
 
-    app = init_app(app)
+    init_app(app)
+
+    with app.app_context():
+        db.session = scoped_session(
+            sessionmaker(
+                bind=db.engine,
+                autocommit=False,
+                autoflush=False,
+                expire_on_commit=False
+            )
+        )
+    
+    # Configuración de manejo automático de sesiones
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        """Cierra la sesión al finalizar cada request"""
+        db.session.remove()
+
     app.register_blueprint(main_bp, url_prefix='/')
     app.register_blueprint(api_bp, url_prefix='/api')
     
--- app/config/settings.py
+++ app/config/settings.py
@@ -6,6 +6,13 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DB_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_size': 10,
+        'max_overflow': 5,
+        'pool_timeout': 30,
+        'pool_recycle': 20,
+        'pool_pre_ping': True
+    }
 
 class TestConfig(Config):
     TESTING = True
